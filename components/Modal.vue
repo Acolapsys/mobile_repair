45dbf5846ede8ee85abfa0b9fbf84808d494154d
Acolapsys <timur_sh@bk.ru>
2021-01-10 14:02:45 +0500

    fix: fix slider in modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,11 +1,11 @@
 <template>
-  <transition :name="animationName">
-    <div v-if="isModalVisible" class="overlay">
+  <div v-if="isModalVisible" class="overlay">
+    <transition name="slide-fade" mode="">
       <div ref="modal" class="modal px-3">
         <slot />
       </div>
-    </div>
-  </transition>
+    </transition>
+  </div>
 </template>
 <script>
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
@@ -16,7 +16,7 @@ export default {
   props: {
     animationName: {
       type: String,
-      default: 'fade-in-right',
+      default: 'slide-fade',
     },
     modalName: {
       type: String,
@@ -35,14 +35,6 @@ export default {
       },
     },
   },
-  //   watch: {
-  //     isModalVisible(value) {
-  //       if (value) {
-  //         disableBodyScroll(this.$refs.modal)
-  //       }
-  //     }
-  //   },
-
   mounted() {
     this.$nextTick(() => {
       disableBodyScroll(this.$refs.modal)
@@ -67,82 +59,19 @@ export default {
   top: 0;
   background-color: #fff;
   width: 1000px;
+  height: 100vh;
   overflow-y: auto;
 }
-// .fade-enter-active,
-// .fade-leave-active {
-//   transition: opacity 0.5s;
-// }
-// .fade-enter,
-// .fade-leave-to {
-//   opacity: 0;
-// }
 
-// $motion-main: cubic-bezier(0.4, 0, 0.2, 1);
-// $duration-main: 0.25s;
-
-// $motion-exit: cubic-bezier(0.4, 0, 1, 1);
-// $duration-exit: 0.2s;
-
-// .fade-in {
-//   &-down-enter-active,
-//   &-up-enter-active,
-//   &-left-enter-active,
-//   &-right-enter-active {
-//     transition: all $duration-main $motion-main;
-//   }
-
-//   &-down-leave-active,
-//   &-up-leave-active,
-//   &-left-leave-active,
-//   &-right-leave-active {
-//     transition: all $duration-exit $motion-exit;
-//   }
-
-//   &-down-enter,
-//   &-down-leave-to,
-//   &-up-enter,
-//   &-up-leave-to,
-//   &-right-enter,
-//   &-right-leave-to,
-//   &-left-enter,
-//   &-left-leave-to {
-//     opacity: 0;
-//   }
-
-//   &-down-enter,
-//   &-down-leave-to {
-//     transform: translateY(-100%);
-//   }
-
-//   &-up-enter,
-//   &-up-leave-to {
-//     transform: translateY(100%);
-//   }
-
-//   &-left-enter,
-//   &-left-leave-to {
-//     transform: translateX(-100%);
-//   }
-
-//   &-right-enter,
-//   &-right-leave-to {
-//     transform: translateX(100%);
-//   }
-// }
-// .transition-element,
-// .transition-element-no-overflow {
-//   transition: max-height 0.5s ease-in-out 0.4s, opacity 0.8s;
-//   max-height: 100000px;
-//   opacity: 1;
-// }
-// .transition-element {
-//     overflow: hidden;
-// }
-
-// .transition-hide {
-//   max-height: 0;
-//   opacity: 0.01;
-//   transition: max-height 0.5s ease-in-out 0.4s, opacity 0.8s;
-// }
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(100%);
+  opacity: 0;
+}
 </style>
