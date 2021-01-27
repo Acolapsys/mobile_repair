66141183a693b0dfa7748c7d6f4966e9fcb23cbe
Acolapsys <timur_sh@bk.ru>
2021-01-27 17:49:40 +0500

    start implement typescript to store

--- store/payments.ts
+++ store/payments.ts
@@ -1,13 +1,50 @@
-export const actions = {
-  async createPayment({ commit, dispatch }, payment) {
-    const companyId = this.getters['company/companyId']
+import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators'
+
+
+
+enum incomeArticle {
+
+}
+enum outcomeArticle {
+  
+}
+
+interface Payment {
+  type: string
+  date: Date
+  amount: number
+  comment: string
+  paymentArticle: incomeArticle | outcomeArticle
+  managerName: string
+  bill: number
+}
+
+
+
+
+
+@Module({
+  name: 'payments',
+  stateFactory: true,
+  namespaced: true
+})
+
+export default class Payments extends VuexModule {
+ 
+    public bill: number = 0
+    public incomeArticles: Array<string> = []
+    public outcomeArticles: Array<string> = []
+
+  @Action
+  public async createPayment( payment: Object) {
+    const companyId = this.context.getters['company/companyId']
     try {
       await this.$fire.firestore
         .collection('companies')
         .doc(companyId)
         .collection('payments')
         .add(payment)
-      await dispatch('payments/updateBill', +payment.bill, { root: true })
+      await this.context.dispatch('payments/updateBill', payment.bill, { root: true })
     } catch (e) {
       commit('setError', e)
       throw e
@@ -94,11 +131,7 @@ export const actions = {
   },
 }
 
-export const state = () => ({
-  bill: null,
-  incomeArticles: [],
-  outcomeArticles: [],
-})
+
 
 export const mutations = {
   setBill(state, bill) {
@@ -114,4 +147,5 @@ export const mutations = {
 
 export const getters = {
   bill: (s) => s.bill,
+}
 }
